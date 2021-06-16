b94e7cd1866a1a27b3156fde42d2b1336531c792
viktorcal395 <viktorcal395@gmail.com>
2021-06-16 17:03:45 -0500

    Update main.py

--- quickstart-automate/main.py
+++ quickstart-automate/main.py
@@ -56,42 +56,7 @@ def init_connection_engine():
         # [END cloud_sql_postgres_sqlalchemy_lifetime]
     }
 
-    if os.environ.get("DB_HOST"):
-        return init_tcp_connection_engine(db_config)
-    else:
-        return init_unix_connection_engine(db_config)
-
-
-def init_tcp_connection_engine(db_config):
-    # [START cloud_sql_postgres_sqlalchemy_create_tcp]
-    # Remember - storing secrets in plaintext is potentially unsafe. Consider using
-    # something like https://cloud.google.com/secret-manager/docs/overview to help keep
-    # secrets secret.
-    db_user = os.environ["DB_USER"]
-    db_pass = os.environ["DB_PASS"]
-    db_name = os.environ["DB_NAME"]
-    db_host = os.environ["DB_HOST"]
-
-    # Extract host and port from db_host
-    host_args = db_host.split(":")
-    db_hostname, db_port = host_args[0], int(host_args[1])
-
-    pool = sqlalchemy.create_engine(
-        # Equivalent URL:
-        # postgres+pg8000://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
-        sqlalchemy.engine.url.URL(
-            drivername="postgresql+pg8000",
-            username=db_user,  # e.g. "my-database-user"
-            password=db_pass,  # e.g. "my-database-password"
-            host=db_hostname,  # e.g. "127.0.0.1"
-            port=db_port,  # e.g. 5432
-            database=db_name  # e.g. "my-database-name"
-        ),
-        **db_config
-    )
-    # [END cloud_sql_postgres_sqlalchemy_create_tcp]
-    pool.dialect.description_encoding = None
-    return pool
+    return init_unix_connection_engine(db_config)
 
 
 def init_unix_connection_engine(db_config):
